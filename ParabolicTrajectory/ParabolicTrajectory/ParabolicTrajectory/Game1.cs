6c119a919b123fb50f560688c8dfead4ea4151d7
surui1996 <surui1996@gmail.com>
2014-02-04 12:25:42 +0200

    prettier visualization of the testing
    todo: add fricition
    do more oop

---
+++ ParabolicTrajectory/ParabolicTrajectory/ParabolicTrajectory/Game1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ParabolicTrajectory
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        Texture2D ball2D;
+        const int BALL_DIAMETER = 51;
+        Texture2D arrow2D;
+        Texture2D ground2D;
+        const int GROUND_LEN = 225;
+
+        Graph graph;
+        float velocity, angle;
+        const float kG = 9.8f, X_POS = 50f, Y_POS = 200f, DEFAULT_VELOCITY = 50f, DT = 0.005f, SCALE = 0.1f;
+        Color DEFAULT_COLOR = Color.CornflowerBlue;
+        bool spaceClicked = false, finishedShot = false;
+        float timeFromShot;
+        int positionIndex;
+        Vector2 origin;
+        List<Vector3> positions;
+        public Game1()
+        {
+            
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            graph = new Graph(GraphicsDevice);
+            positions = new List<Vector3>();
+            velocity = DEFAULT_VELOCITY;
+            angle = 45.0f;
+            spaceClicked = false;
+            origin = new Vector2(X_POS, Y_POS);
+
+
+            CalcTrajectory(velocity, angle);
+            
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("font");
+            ball2D = Content.Load<Texture2D>("transparent ball");
+            arrow2D = Content.Load<Texture2D>("transparent arrow");
+            ground2D = Content.Load<Texture2D>("ground");
+        }
+
+        private void CalcTrajectory(float velocity, float angle)
+        {
+            float angleRad = MathHelper.ToRadians(angle);
+            
+            float vx = velocity * (float)Math.Cos(angleRad);
+            float vy =  velocity * (float)Math.Sin(angleRad);
+            Vector3 pos = Vector3.Zero;
+            
+            for (float t = 0; t < 100.0f; t += DT)
+            {
+                pos = new Vector3(origin, 0) + new Vector3(vx * t, -vy * t + kG * t * t, 0);
+                positions.Add(pos);
+                //if (HasBallReachedGround((int)(t / DT)))
+                //    break;
+            }
+            
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            // Allows the game to exit
+            if (state.IsKeyDown(Keys.Escape))
+                this.Exit();
+            else if (state.IsKeyDown(Keys.W))
+                angle += 0.05f;
+            else if (state.IsKeyDown(Keys.S))
+                angle -= 0.05f;
+            else if (state.IsKeyDown(Keys.D))
+                velocity += 0.5f;
+            else if (state.IsKeyDown(Keys.A))
+                velocity -= 0.5f;
+
+            if (state.IsKeyDown(Keys.Space) && !spaceClicked)
+            {
+                positions = new List<Vector3>();
+                CalcTrajectory(velocity, angle);
+                spaceClicked = true;
+                finishedShot = false;
+                timeFromShot = 0;
+            }
+            else if (!state.IsKeyDown(Keys.Space) && spaceClicked)
+                spaceClicked = false;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(DEFAULT_COLOR);
+
+            spriteBatch.Begin();
+
+            DrawShootingParameters();
+            DrawMovingBall(gameTime.ElapsedGameTime.Milliseconds / 1000f);
+            DrawGround();
+
+            spriteBatch.End();
+
+            graph.Draw(positions, Color.Black);
+            base.Draw(gameTime);
+        }
+
+        private void DrawShootingParameters()
+        {
+            spriteBatch.DrawString(font, "velocity: " + Math.Round(velocity).ToString() + "m/s", new Vector2(600, 30), Color.Red);
+            spriteBatch.DrawString(font, "angle: " + angle.ToString(), new Vector2(600, 60), Color.Red);
+            spriteBatch.Draw(arrow2D, origin, null, Color.CornflowerBlue, -MathHelper.ToRadians(angle),
+                new Vector2(0f, 55f), new Vector2(SCALE * 0.8f * velocity / DEFAULT_VELOCITY, SCALE), SpriteEffects.None, 0);
+        }
+
+        private void DrawMovingBall(float ellapsedSeconds)
+        {
+            if (!finishedShot)
+            {
+                positionIndex = (int)(timeFromShot / DT);
+                spriteBatch.Draw(ball2D, new Vector2(positions[positionIndex].X, positions[positionIndex].Y), null, DEFAULT_COLOR, 0f,
+                    new Vector2(51f, 51f), SCALE, SpriteEffects.None, 0);
+                if (HasBallReachedGround(positionIndex))
+                    finishedShot = true;
+
+                timeFromShot += ellapsedSeconds;
+            }
+            else
+                spriteBatch.Draw(ball2D, new Vector2(positions[positionIndex].X, positions[positionIndex].Y), null, DEFAULT_COLOR, 0f,
+                    new Vector2(BALL_DIAMETER, BALL_DIAMETER), SCALE, SpriteEffects.None, 0);
+        }
+
+        private void DrawGround()
+        {
+            float len = ((float)GROUND_LEN * SCALE);
+            for (int i = 0; i <= (int)((float)(graphics.PreferredBackBufferWidth) / len); i++)
+                spriteBatch.Draw(ground2D, new Vector2(i * len, Y_POS), null, Color.White, 0f, new Vector2(0, 0),
+                    SCALE, SpriteEffects.None, 0);
+        }
+
+        private bool HasBallReachedGround(int positionIndex)
+        {
+            return positionIndex > 10 && Math.Abs(positions[positionIndex].Y - Y_POS) < 1f;
+        }
+
+    }
+}
---
+++ ParabolicTrajectory/ParabolicTrajectory/ParabolicTrajectory/Graph.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace ParabolicTrajectory
+{
+    public class Graph
+    {
+
+        /// <summary>
+        /// The bottom left position of the graph
+        /// </summary>
+        public Vector2 Position { get; set; }
+        /// <summary>
+        /// The size of the graph.
+        /// The graph values will be scaled horizontally to fill width (Size.X)
+        /// Vertically, the values will be scaled based on MaxValue property, where the position of the value that is equal to MaxValue will be Size.Y
+        /// </summary>
+        public Point Size { get; set; }
+
+        /// <summary>
+        /// Determines the vertical scaling of the graph.
+        /// The value that is equal to MaxValue will be displayed at the top of the graph (at point Size.Y)
+        /// </summary>
+        public float MaxValue { get; set; }
+
+        private Vector2 _scale = new Vector2(1.0f, 1.0f);
+
+        BasicEffect _effect;
+        short[] lineListIndices;
+
+        public Graph(GraphicsDevice graphicsDevice)
+        {
+            _effect = new BasicEffect(graphicsDevice);
+            _effect.View = Matrix.CreateLookAt(Vector3.Backward, Vector3.Zero, Vector3.Up);
+            _effect.Projection = Matrix.CreateOrthographicOffCenter(0, (float)graphicsDevice.Viewport.Width, (float)graphicsDevice.Viewport.Height, 0, 1.0f, 1000.0f);
+            _effect.World = Matrix.Identity;
+
+            _effect.VertexColorEnabled = true;
+        }
+
+        //public Graph(GraphicsDevice graphicsDevice, Point size)
+        //{
+        //    _effect = new BasicEffect(graphicsDevice);
+        //    _effect.View = Matrix.CreateLookAt(Vector3.Backward, Vector3.Zero, Vector3.Up);
+        //    _effect.Projection = Matrix.CreateOrthographicOffCenter(0, (float)graphicsDevice.Viewport.Width, (float)graphicsDevice.Viewport.Height, 0, 1.0f, 1000.0f);
+        //    _effect.World = Matrix.Identity;
+
+        //    _effect.VertexColorEnabled = true;
+
+        //    this.MaxValue = 1;
+        //    this.Size = size;
+        //    if (size.Y <= 0)
+        //        Size = new Point(size.X, 1);
+        //    if (size.X <= 0)
+        //        Size = new Point(1, size.Y);
+        //}
+
+        void UpdateWorld()
+        {
+            _effect.World = Matrix.CreateScale(_scale.X, _scale.Y, 1.0f)
+                          * Matrix.CreateRotationX(MathHelper.Pi) //flips the graph so that the higher values are above. Makes bottom left the graph origin.
+                          * Matrix.CreateTranslation(new Vector3(this.Position, 0));
+        }
+
+        /// <summary>
+        /// Draws the values in given order, in specified color
+        /// </summary>
+        /// <param name="values">Values to draw, in order from left to right</param>
+        /// <param name="color">Color of the entire graph</param>
+        public void Draw(List<Vector3> values, Color color)
+        {
+            if (values.Count < 2)
+                return;
+
+            //float xScale = this.Size.X / (float)values.Count;
+            //float yScale = this.Size.Y / MaxValue;
+
+            //_scale = new Vector2(xScale, yScale);
+            //UpdateWorld();
+
+            VertexPositionColor[] pointList = new VertexPositionColor[values.Count];
+            for (int i = 0; i < values.Count; i++)
+                pointList[i] = new VertexPositionColor(values[i], color);
+
+            DrawLineList(pointList);
+
+        }
+
+        void DrawLineList(VertexPositionColor[] pointList)
+        {
+            int numOfLines = pointList.Length - 1;
+
+            //indices updated only need to be updated when the number of points has changed
+            if (lineListIndices == null || lineListIndices.Length != ((pointList.Length * 2) - 2))
+            {
+                lineListIndices = new short[numOfLines * 2];
+                for (int i = 0; i < numOfLines; i++)
+                {
+                    lineListIndices[i * 2] = (short)(i);
+                    lineListIndices[(i * 2) + 1] = (short)(i + 1);
+                }
+            }
+
+            foreach (EffectPass pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                _effect.GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(
+                    PrimitiveType.LineList,
+                    pointList,
+                    0,
+                    pointList.Length,
+                    lineListIndices,
+                    0,
+                    numOfLines
+                );
+            }
+        }
+    }
+}
